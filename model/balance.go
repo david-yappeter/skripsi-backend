1aec576eb55a484239c698c8b224be3b0af8e117
David Yappeter <davidyap11les@gmail.com>
2024-01-12 10:19:45 +0700

    feat: add balance model

---
+++ model/balance.go
@@ -0,0 +1,56 @@
+package model
+
+const BalanceTableName = "balances"
+
+type Balance struct {
+	Id            string  `db:"id"`
+	AccountNumber string  `db:"account_number"`
+	AccountName   string  `db:"account_name"`
+	BankName      string  `db:"bank_name"`
+	Name          string  `db:"name"`
+	Amount        float64 `db:"amount"`
+
+	Timestamp
+}
+
+func (m *Balance) TableName() string {
+	return BalanceTableName
+}
+
+func (m *Balance) TableIds() []string {
+	return []string{"id"}
+}
+
+func (m *Balance) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":           m.Id,
+		"account_name": m.AccountName,
+		"bank_name":    m.BankName,
+		"name":         m.Name,
+		"amount":       m.Amount,
+		"created_at":   m.CreatedAt,
+		"updated_at":   m.UpdatedAt,
+	}
+}
+
+type BalanceQueryOption struct {
+	QueryOption
+
+	Phrase *string
+}
+
+var _ PrepareOption = &BalanceQueryOption{}
+
+func (o *BalanceQueryOption) SetDefaultFields() {
+	if len(o.Fields) == 0 {
+		o.Fields = []string{"u.*"}
+	}
+}
+
+func (o *BalanceQueryOption) SetDefaultSorts() {
+	if len(o.Sorts) == 0 {
+		o.Sorts = Sorts{
+			{Field: "updated_at", Direction: "desc"},
+		}
+	}
+}
