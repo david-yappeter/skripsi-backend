6c522f4134a88ef91859e8703a88cb5ebaf67bc6
David Yappeter <davidyap11les@gmail.com>
2024-01-16 10:19:00 +0700

    feat: add product_receive_item model

---
+++ model/product_receive_items.go
@@ -0,0 +1,35 @@
+package model
+
+const ProductReceiveItemTableName = "product_receive_items"
+
+type ProductReceiveItem struct {
+	Id               string  `db:"id"`
+	ProductReceiveId string  `db:"product_receive_id"`
+	ProductUnitId    string  `db:"product_unit_id"`
+	UserId           string  `db:"user_id"`
+	Qty              float64 `db:"qty"`
+	PricePerUnit     float64 `db:"price_per_unit"`
+
+	Timestamp
+}
+
+func (m *ProductReceiveItem) TableName() string {
+	return ProductReceiveItemTableName
+}
+
+func (m *ProductReceiveItem) TableIds() []string {
+	return []string{"id"}
+}
+
+func (m *ProductReceiveItem) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":                 m.Id,
+		"product_receive_id": m.ProductReceiveId,
+		"product_unit_id":    m.ProductUnitId,
+		"user_id":            m.UserId,
+		"qty":                m.Qty,
+		"price_per_unit":     m.PricePerUnit,
+		"created_at":         m.CreatedAt,
+		"updated_at":         m.UpdatedAt,
+	}
+}
