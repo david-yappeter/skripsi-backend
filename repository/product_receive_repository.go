ee3a1553b0f6e7e74324fdb3bd87ab6f4f72a9da
David Yappeter <davidyap11les@gmail.com>
2024-01-16 10:19:15 +0700

    feat: add product_receive repository

---
+++ repository/product_receive_repository.go
@@ -0,0 +1,141 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"myapp/data_type"
+	"myapp/infrastructure"
+	"myapp/model"
+
+	"github.com/Masterminds/squirrel"
+)
+
+type ProductReceiveRepository interface {
+	// create
+	Insert(ctx context.Context, productReceive *model.ProductReceive) error
+	InsertMany(ctx context.Context, productReceives []model.ProductReceive, options ...data_type.RepositoryOption) error
+
+	// read
+	Count(ctx context.Context, options ...model.ProductReceiveQueryOption) (int, error)
+	Fetch(ctx context.Context, options ...model.ProductReceiveQueryOption) ([]model.ProductReceive, error)
+	Get(ctx context.Context, id string) (*model.ProductReceive, error)
+
+	// update
+	Update(ctx context.Context, productReceive *model.ProductReceive) error
+
+	// delete
+	Delete(ctx context.Context, productReceive *model.ProductReceive) error
+	Truncate(ctx context.Context) error
+}
+
+type productReceiveRepository struct {
+	db          infrastructure.DBTX
+	loggerStack infrastructure.LoggerStack
+}
+
+func NewProductReceiveRepository(
+	db infrastructure.DBTX,
+	loggerStack infrastructure.LoggerStack,
+) ProductReceiveRepository {
+	return &productReceiveRepository{
+		db:          db,
+		loggerStack: loggerStack,
+	}
+}
+
+func (r *productReceiveRepository) fetch(ctx context.Context, stmt squirrel.Sqlizer) ([]model.ProductReceive, error) {
+	productReceives := []model.ProductReceive{}
+	if err := fetch(r.db, ctx, &productReceives, stmt); err != nil {
+		return nil, err
+	}
+
+	return productReceives, nil
+}
+
+func (r *productReceiveRepository) get(ctx context.Context, stmt squirrel.Sqlizer) (*model.ProductReceive, error) {
+	productReceive := model.ProductReceive{}
+	if err := get(r.db, ctx, &productReceive, stmt); err != nil {
+		return nil, err
+	}
+
+	return &productReceive, nil
+}
+
+func (r *productReceiveRepository) prepareQuery(option model.ProductReceiveQueryOption) squirrel.SelectBuilder {
+	stmt := stmtBuilder.Select().
+		From(fmt.Sprintf("%s u", model.ProductReceiveTableName))
+
+	andStatements := squirrel.And{}
+
+	if option.Phrase != nil {
+		phrase := "%" + *option.Phrase + "%"
+		andStatements = append(
+			andStatements,
+			squirrel.Or{
+				squirrel.ILike{"u.name": phrase},
+			},
+		)
+	}
+
+	stmt = stmt.Where(andStatements)
+
+	stmt = model.Prepare(stmt, &option)
+
+	return stmt
+}
+
+func (r *productReceiveRepository) Insert(ctx context.Context, productReceive *model.ProductReceive) error {
+	return defaultInsert(r.db, ctx, productReceive, "*")
+}
+
+func (r *productReceiveRepository) InsertMany(ctx context.Context, productReceives []model.ProductReceive, options ...data_type.RepositoryOption) error {
+	arr := []model.BaseModel{}
+	for i := range productReceives {
+		arr = append(arr, &productReceives[i])
+	}
+
+	return defaultInsertMany(r.db, ctx, arr, "*")
+}
+
+func (r *productReceiveRepository) Count(ctx context.Context, options ...model.ProductReceiveQueryOption) (int, error) {
+	option := model.ProductReceiveQueryOption{}
+	if len(options) > 0 {
+		option = options[0]
+	}
+	option.IsCount = true
+
+	stmt := r.prepareQuery(option)
+
+	return count(r.db, ctx, stmt)
+}
+
+func (r *productReceiveRepository) Fetch(ctx context.Context, options ...model.ProductReceiveQueryOption) ([]model.ProductReceive, error) {
+	option := model.ProductReceiveQueryOption{}
+	if len(options) > 0 {
+		option = options[0]
+	}
+
+	stmt := r.prepareQuery(option)
+
+	return r.fetch(ctx, stmt)
+}
+
+func (r *productReceiveRepository) Get(ctx context.Context, id string) (*model.ProductReceive, error) {
+	stmt := stmtBuilder.Select("*").
+		From(model.ProductReceiveTableName).
+		Where(squirrel.Eq{"id": id})
+
+	return r.get(ctx, stmt)
+}
+
+func (r *productReceiveRepository) Update(ctx context.Context, productReceive *model.ProductReceive) error {
+	return defaultUpdate(r.db, ctx, productReceive, "*", nil)
+}
+
+func (r *productReceiveRepository) Delete(ctx context.Context, productReceive *model.ProductReceive) error {
+	return defaultDestroy(r.db, r.loggerStack, ctx, productReceive, nil)
+}
+
+func (r *productReceiveRepository) Truncate(ctx context.Context) error {
+	return truncate(r.db, ctx, model.ProductReceiveTableName)
+}
