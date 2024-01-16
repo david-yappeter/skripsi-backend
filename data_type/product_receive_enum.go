96309ddc4d001d43c4462d93fab5424544057bf6
David Yappeter <davidyap11les@gmail.com>
2024-01-16 10:18:34 +0700

    feat: add product_receive data_type

---
+++ data_type/product_receive_enum.go
@@ -0,0 +1,9 @@
+package data_type
+
+//go:generate go run myapp/tool/stringer -linecomment -type=ProductReceiveStatus -output=product_receive_enum_gen.go -swagoutput=../tool/swag/enum_gen/product_receive_enum_gen.go -custom
+type ProductReceiveStatus int // @name ProductReceiveStatusEnum
+
+const (
+	ProductReceiveStatusPending   ProductReceiveStatus = iota + 1 // PENDING
+	ProductReceiveStatusCompleted                                 // COMPLETED
+)
