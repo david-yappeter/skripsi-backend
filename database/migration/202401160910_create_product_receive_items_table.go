efa9c1267a745fa893c1d92413f762124350247d
David Yappeter <davidyap11les@gmail.com>
2024-01-16 10:17:57 +0700

    feat: add product_receive_items migration

---
+++ database/migration/202401160910_create_product_receive_items_table.go
@@ -0,0 +1,26 @@
+package migration
+
+func init() {
+	sourceDriver.append(
+		202401160910,
+		`
+			CREATE TABLE IF NOT EXISTS product_receive_items (
+				id char(36) NOT NULL,
+				product_receive_id char(36) NOT NULL,
+				product_unit_id char(36) NOT NULL,
+				user_id char(36) NOT NULL,
+				qty decimal(16,2) NOT NULL,
+				price_per_unit decimal(16,2) NOT NULL,
+				created_at timestamp NOT NULL,
+				updated_at timestamp NOT NULL,
+				CONSTRAINT product_receive_items_pk PRIMARY KEY (id),
+				CONSTRAINT product_receive_items_product_receives_fk FOREIGN KEY (product_receive_id) REFERENCES product_receives (id),
+				CONSTRAINT product_receive_items_product_units_fk FOREIGN KEY (product_unit_id) REFERENCES product_units (id),
+				CONSTRAINT product_receive_items_users_fk FOREIGN KEY (user_id) REFERENCES users (id)
+			);
+		`,
+		`
+			DROP TABLE IF EXISTS product_receive_items;
+		`,
+	)
+}
