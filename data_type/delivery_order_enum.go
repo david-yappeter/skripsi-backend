8c744e6bb4d5f5984df3c84b27f48ea6c73670a4
David Yappeter <davidyap11les@gmail.com>
2024-01-19 17:33:56 +0700

    feat: add delivery_order enum

---
+++ data_type/delivery_order_enum.go
@@ -0,0 +1,9 @@
+package data_type
+
+//go:generate go run myapp/tool/stringer -linecomment -type=DeliveryOrderStatus -output=delivery_order_enum_gen.go -swagoutput=../tool/swag/enum_gen/delivery_order_enum_gen.go -custom
+type DeliveryOrderStatus int // @name DeliveryOrderStatusEnum
+
+const (
+	DeliveryOrderStatusPending   DeliveryOrderStatus = iota + 1 // PENDING
+	DeliveryOrderStatusCompleted                                // COMPLETED
+)
