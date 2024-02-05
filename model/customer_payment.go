482c4aa0c84b5bd817404c08abeb45f7c1b3a17b
David Yappeter <davidyap11les@gmail.com>
2024-02-05 17:32:39 +0700

    feat: add customer_payment model and repository

---
+++ model/customer_payment.go
@@ -0,0 +1,59 @@
+package model
+
+import "myapp/data_type"
+
+const CustomerPaymentTableName = "customer_payments"
+
+type CustomerPayment struct {
+	Id             string             `db:"id"`
+	UserId         string             `db:"user_id"`
+	CustomerDebtId string             `db:"customer_debt_id"`
+	Amount         float64            `db:"amount"`
+	Description    *string            `db:"description"`
+	PaidAt         data_type.DateTime `db:"paid_at"`
+
+	Timestamp
+}
+
+func (m *CustomerPayment) TableName() string {
+	return CustomerPaymentTableName
+}
+
+func (m *CustomerPayment) TableIds() []string {
+	return []string{"id"}
+}
+
+func (m *CustomerPayment) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":               m.Id,
+		"user_id":          m.UserId,
+		"customer_debt_id": m.CustomerDebtId,
+		"amount":           m.Amount,
+		"description":      m.Description,
+		"paid_at":          m.PaidAt,
+		"created_at":       m.CreatedAt,
+		"updated_at":       m.UpdatedAt,
+	}
+}
+
+type CustomerPaymentQueryOption struct {
+	QueryOption
+
+	Phrase *string
+}
+
+var _ PrepareOption = &CustomerPaymentQueryOption{}
+
+func (o *CustomerPaymentQueryOption) SetDefaultFields() {
+	if len(o.Fields) == 0 {
+		o.Fields = []string{"u.*"}
+	}
+}
+
+func (o *CustomerPaymentQueryOption) SetDefaultSorts() {
+	if len(o.Sorts) == 0 {
+		o.Sorts = Sorts{
+			{Field: "updated_at", Direction: "desc"},
+		}
+	}
+}
---
+++ repository/customer_payment_repository.go
@@ -0,0 +1,134 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"myapp/data_type"
+	"myapp/infrastructure"
+	"myapp/model"
+
+	"github.com/Masterminds/squirrel"
+)
+
+type CustomerPaymentRepository interface {
+	// create
+	Insert(ctx context.Context, customerPayment *model.CustomerPayment) error
+	InsertMany(ctx context.Context, customerPayments []model.CustomerPayment, options ...data_type.RepositoryOption) error
+
+	// read
+	Count(ctx context.Context, options ...model.CustomerPaymentQueryOption) (int, error)
+	Fetch(ctx context.Context, options ...model.CustomerPaymentQueryOption) ([]model.CustomerPayment, error)
+	Get(ctx context.Context, id string) (*model.CustomerPayment, error)
+
+	// update
+	Update(ctx context.Context, customerPayment *model.CustomerPayment) error
+
+	// delete
+	Delete(ctx context.Context, customerPayment *model.CustomerPayment) error
+	Truncate(ctx context.Context) error
+}
+
+type customerPaymentRepository struct {
+	db          infrastructure.DBTX
+	loggerStack infrastructure.LoggerStack
+}
+
+func NewCustomerPaymentRepository(
+	db infrastructure.DBTX,
+	loggerStack infrastructure.LoggerStack,
+) CustomerPaymentRepository {
+	return &customerPaymentRepository{
+		db:          db,
+		loggerStack: loggerStack,
+	}
+}
+
+func (r *customerPaymentRepository) fetch(ctx context.Context, stmt squirrel.Sqlizer) ([]model.CustomerPayment, error) {
+	customerPayments := []model.CustomerPayment{}
+	if err := fetch(r.db, ctx, &customerPayments, stmt); err != nil {
+		return nil, err
+	}
+
+	return customerPayments, nil
+}
+
+func (r *customerPaymentRepository) get(ctx context.Context, stmt squirrel.Sqlizer) (*model.CustomerPayment, error) {
+	customerPayment := model.CustomerPayment{}
+	if err := get(r.db, ctx, &customerPayment, stmt); err != nil {
+		return nil, err
+	}
+
+	return &customerPayment, nil
+}
+
+func (r *customerPaymentRepository) prepareQuery(option model.CustomerPaymentQueryOption) squirrel.SelectBuilder {
+	stmt := stmtBuilder.Select().
+		From(fmt.Sprintf("%s u", model.CustomerPaymentTableName))
+
+	if option.Phrase != nil {
+		phrase := "%" + *option.Phrase + "%"
+		stmt = stmt.Where(squirrel.Or{
+			squirrel.ILike{"u.name": phrase},
+		})
+	}
+
+	stmt = model.Prepare(stmt, &option)
+
+	return stmt
+}
+
+func (r *customerPaymentRepository) Insert(ctx context.Context, customerPayment *model.CustomerPayment) error {
+	return defaultInsert(r.db, ctx, customerPayment, "*")
+}
+
+func (r *customerPaymentRepository) InsertMany(ctx context.Context, customerPayments []model.CustomerPayment, options ...data_type.RepositoryOption) error {
+	arr := []model.BaseModel{}
+	for i := range customerPayments {
+		arr = append(arr, &customerPayments[i])
+	}
+
+	return defaultInsertMany(r.db, ctx, arr, "*")
+}
+
+func (r *customerPaymentRepository) Count(ctx context.Context, options ...model.CustomerPaymentQueryOption) (int, error) {
+	option := model.CustomerPaymentQueryOption{}
+	if len(options) > 0 {
+		option = options[0]
+	}
+	option.IsCount = true
+
+	stmt := r.prepareQuery(option)
+
+	return count(r.db, ctx, stmt)
+}
+
+func (r *customerPaymentRepository) Fetch(ctx context.Context, options ...model.CustomerPaymentQueryOption) ([]model.CustomerPayment, error) {
+	option := model.CustomerPaymentQueryOption{}
+	if len(options) > 0 {
+		option = options[0]
+	}
+
+	stmt := r.prepareQuery(option)
+
+	return r.fetch(ctx, stmt)
+}
+
+func (r *customerPaymentRepository) Get(ctx context.Context, id string) (*model.CustomerPayment, error) {
+	stmt := stmtBuilder.Select("*").
+		From(model.CustomerPaymentTableName).
+		Where(squirrel.Eq{"id": id})
+
+	return r.get(ctx, stmt)
+}
+
+func (r *customerPaymentRepository) Update(ctx context.Context, customerPayment *model.CustomerPayment) error {
+	return defaultUpdate(r.db, ctx, customerPayment, "*", nil)
+}
+
+func (r *customerPaymentRepository) Delete(ctx context.Context, customerPayment *model.CustomerPayment) error {
+	return defaultDestroy(r.db, r.loggerStack, ctx, customerPayment, nil)
+}
+
+func (r *customerPaymentRepository) Truncate(ctx context.Context) error {
+	return truncate(r.db, ctx, model.CustomerPaymentTableName)
+}
