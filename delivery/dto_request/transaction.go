ca8654d9a275baaa262f834955833e8a5280ce4f
David Yappeter <davidyap11les@gmail.com>
2024-02-26 11:32:46 +0700

    fix: add missing dto_request

--- delivery/dto_request/transaction.go
+++ delivery/dto_request/transaction.go
@@ -1,4 +1,8 @@
 package dto_request
 
+import "myapp/data_type"
+
 type TransactionCheckoutCartRequest struct {
+	PaymentType     data_type.TransactionPaymentType `json:"payment_type" validate:"required,data_type_enum"`
+	ReferenceNumber *string                          `json:"reference_number" validate:"omitempty,not_empty"`
 } // @name TransactionCheckoutCartRequest
