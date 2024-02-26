f93eebf5d80cbce570d1562cf2b3248a1f4a0fef
David Yappeter <davidyap11les@gmail.com>
2024-02-26 16:04:27 +0700

    feat: add product_stock_mutation model

---
+++ model/product_stock_mutation.go
@@ -0,0 +1,57 @@
+package model
+
+const ProductStockMutationTableName = "product_stock_mutations"
+
+type ProductStockMutation struct {
+	Id            string  `json:"id"`
+	ProductUnitId string  `json:"product_unit_id"`
+	Qty           float64 `json:"qty"`
+	ScaleToBase   float64 `json:"scale_to_base"`
+	BaseQtyLeft   float64 `json:"base_qty_left"`
+	BaseCostPrice float64 `json:"base_cost_price"`
+
+	Timestamp
+}
+
+func (m *ProductStockMutation) TableName() string {
+	return ProductStockMutationTableName
+}
+
+func (m *ProductStockMutation) TableIds() []string {
+	return []string{"id"}
+}
+
+func (m *ProductStockMutation) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":              m.Id,
+		"product_unit_id": m.ProductUnitId,
+		"qty":             m.Qty,
+		"scale_to_base":   m.ScaleToBase,
+		"base_qty_left":   m.BaseQtyLeft,
+		"base_cost_price": m.BaseCostPrice,
+		"created_at":      m.CreatedAt,
+		"updated_at":      m.UpdatedAt,
+	}
+}
+
+type ProductStockMutationQueryOption struct {
+	QueryOption
+
+	Phrase *string
+}
+
+var _ PrepareOption = &ProductStockMutationQueryOption{}
+
+func (o *ProductStockMutationQueryOption) SetDefaultFields() {
+	if len(o.Fields) == 0 {
+		o.Fields = []string{"psm.*"}
+	}
+}
+
+func (o *ProductStockMutationQueryOption) SetDefaultSorts() {
+	if len(o.Sorts) == 0 {
+		o.Sorts = Sorts{
+			{Field: "updated_at", Direction: "desc"},
+		}
+	}
+}
