15ee879ec0ba89d590e32c3ac09e32bb28eef47d
David Yappeter <davidyap11les@gmail.com>
2024-03-20 16:45:57 +0700

    feat: add delivery_order_position model

---
+++ model/delivery_order_position.go
@@ -0,0 +1,55 @@
+package model
+
+const DeliveryOrderPositionTableName = "delivery_order_positions"
+
+type DeliveryOrderPosition struct {
+	Id              string  `db:"id"`
+	DeliveryOrderId string  `db:"delivery_order_id"`
+	DriverUserId    string  `db:"driver_user_id"`
+	Latitude        float64 `db:"latitude"`
+	Longitude       float64 `db:"longitude"`
+
+	Timestamp
+}
+
+func (m *DeliveryOrderPosition) TableName() string {
+	return DeliveryOrderPositionTableName
+}
+
+func (m *DeliveryOrderPosition) TableIds() []string {
+	return []string{"id"}
+}
+
+func (m *DeliveryOrderPosition) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"id":                m.Id,
+		"delivery_order_id": m.DeliveryOrderId,
+		"driver_user_id":    m.DriverUserId,
+		"latitude":          m.Latitude,
+		"longitude":         m.Longitude,
+		"created_at":        m.CreatedAt,
+		"updated_at":        m.UpdatedAt,
+	}
+}
+
+type DeliveryOrderPositionQueryOption struct {
+	QueryOption
+
+	Phrase *string
+}
+
+var _ PrepareOption = &DeliveryOrderPositionQueryOption{}
+
+func (o *DeliveryOrderPositionQueryOption) SetDefaultFields() {
+	if len(o.Fields) == 0 {
+		o.Fields = []string{"dop.*"}
+	}
+}
+
+func (o *DeliveryOrderPositionQueryOption) SetDefaultSorts() {
+	if len(o.Sorts) == 0 {
+		o.Sorts = Sorts{
+			{Field: "updated_at", Direction: "desc"},
+		}
+	}
+}
