885fec24919135c8cf5db93d748c6270e2f19204
David Yappeter <davidyap11les@gmail.com>
2024-03-22 09:16:01 +0700

    feat: load discount on customer_type dto_response

--- delivery/dto_response/customer_type.go
+++ delivery/dto_response/customer_type.go
@@ -7,6 +7,8 @@ type CustomerTypeResponse struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description" extensions:"x-nullable"`
 	Timestamp
+
+	Discounts []CustomerTypeDiscountResponse `json:"discounts" extensions:"x-nullable"`
 } // @name CustomerTypeResponse
 
 func NewCustomerTypeResponse(customerType model.CustomerType) CustomerTypeResponse {
@@ -15,6 +17,10 @@ func NewCustomerTypeResponse(customerType model.CustomerType) CustomerTypeRespon
 		Name:        customerType.Name,
 		Description: customerType.Description,
 		Timestamp:   Timestamp(customerType.Timestamp),
+	}
+
+	for _, discount := range customerType.CustomerTypeDiscounts {
+		r.Discounts = append(r.Discounts, NewCustomerTypeDiscountResponse(discount))
 	}
 
 	return r
