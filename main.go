22f14e2f6d6a7b7f2c4158fd04ff1a35cecd88d6
David Yappeter <davidyap11les@gmail.com>
2024-04-20 13:48:17 +0700

    fix: main.go testing code not removed

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"myapp/cmd"
-
-	goShopee "github.com/david-yappeter/go-shopee-v2"
 )
 
 // @title		Mortal Health - Clinic Pilot API
@@ -11,17 +9,5 @@ import (
 // @host		cp-api.mortalhealth.com
 // @BasePath	/
 func main() {
-	goShopee.NewClient(goShopee.App{
-		PartnerID:   0,
-		PartnerKey:  "",
-		RedirectURL: "",
-		APIURL:      "",
-		Client:      &goShopee.Client{},
-	}, goShopee.WithRetry(3), goShopee.WithLogger(&goShopee.LeveledLogger{
-		Level: goShopee.LevelDebug,
-	}))
-
-	return
-
 	cmd.Execute()
 }
