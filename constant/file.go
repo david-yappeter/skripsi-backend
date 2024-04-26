357c8099d489864fed90d3531225a2e3edaff641
David Yappeter <davidyap11les@gmail.com>
2024-04-26 11:15:30 +0700

    fix: file path

--- constant/file.go
+++ constant/file.go
@@ -5,9 +5,9 @@ const PrivatePath = "private"
 const (
 	ProductImagePath         = "product"
 	ProductReceiveImagePath  = "product_receive"
-	DeliveryOrderImagePath   = " delivery_order"
-	CustomerPaymentImagePath = " customer_payment"
-	DebtPaymentImagePath     = " debt_payment"
+	DeliveryOrderImagePath   = "delivery_order"
+	CustomerPaymentImagePath = "customer_payment"
+	DebtPaymentImagePath     = "debt_payment"
 
 	ReportPath             = PrivatePath + "/report"
 	ReportProductStockPath = ReportPath + "/product-stock"
