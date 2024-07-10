6c5cb6b681796ede1ade312a34c2fcfdd952eb55
David Yappeter <davidyap11les@gmail.com>
2024-07-10 09:44:45 +0700

    feat: add base_cost_price to product stock response

--- delivery/dto_response/product_stock.go
+++ delivery/dto_response/product_stock.go
@@ -3,19 +3,21 @@ package dto_response
 import "myapp/model"
 
 type ProductStockResponse struct {
-	Id        string  `json:"id"`
-	ProductId string  `json:"product_id"`
-	Qty       float64 `json:"qty"`
+	Id            string  `json:"id"`
+	ProductId     string  `json:"product_id"`
+	Qty           float64 `json:"qty"`
+	BaseCostPrice float64 `json:"base_cost_price"`
 
 	Timestamp
 } // @name ProductStockResponse
 
 func NewProductStockResponse(productStock model.ProductStock) ProductStockResponse {
 	r := ProductStockResponse{
-		Id:        productStock.Id,
-		ProductId: productStock.ProductId,
-		Qty:       productStock.Qty,
-		Timestamp: Timestamp(productStock.Timestamp),
+		Id:            productStock.Id,
+		ProductId:     productStock.ProductId,
+		Qty:           productStock.Qty,
+		BaseCostPrice: productStock.BaseCostPrice,
+		Timestamp:     Timestamp(productStock.Timestamp),
 	}
 
 	return r
